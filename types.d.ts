31fd22779c09351fb5326f874176fa4c78776094
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-08-29 11:20:47 +0200

    Removed unused type.

--- types.d.ts
+++ types.d.ts
@@ -19,9 +19,6 @@ interface Object {
 	[key: string]: any;
 }
 
-// https://stackoverflow.com/a/49286056 | CC BY-SA 3.0
-type ValueOf<T> = T[keyof T];
-
 interface ComponentProps extends Object {
 	children?: React.ReactNode;
 	id?: string | null;
