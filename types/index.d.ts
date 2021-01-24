5a14caf30aa09a5351c175df2e42dcd0f082f34f
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-01-24 18:40:57 +0100

    Renamed and updated types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,3 @@
-// Console log shortcut
-declare const l: Function;
-
-interface Object {
-	[key: string]: any;
-}
-
-interface ComponentProps extends Object {
-	children?: React.ReactNode;
-	id?: string | null;
-	className?: string | null | (string | null)[] | undefined;
-}
-
 interface Attributes {
 	title_html: "h1" | "h1_a" | "a" | "a_h1" | "span";
 	description_enabled: boolean;
@@ -18,12 +5,19 @@ interface Attributes {
 }
 
 interface EditProps {
+	[key: string]: unknown;
 	className: string;
 	attributes: Attributes;
-	setAttributes: Function;
+	setAttributes: (attributes: Partial<Attributes>) => void;
 }
 
 interface SiteInfo {
 	title: string;
 	description: string;
+}
+
+// CSS modules
+declare module "*.styl" {
+	const styles: { [className: string]: string };
+	export default styles;
 }
